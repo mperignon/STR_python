eced707da73e64edfc831bcda4515e144f25d603
Mariela Perignon <mariela@mac1-16-220-dhcp.int.colorado.edu>
2015-10-27 17:42:16 -0600

    working on Acronym1

--- GSD_calculator.py
+++ GSD_calculator.py
@@ -2,7 +2,85 @@ from math import sqrt, log
 import numpy as np
 
 
+def pre_process_data(datapoints):
 
+    dtype = [('diameters', float), ('fractions', float)]
+    datapts = np.array(datapoints, dtype=dtype)
+
+    # scale
+    if max(datapts['fractions']) > 1.:
+        datapts['fractions'] = datapts['fractions'] / 100.
+
+    # sort
+    datapts = np.sort(datapts, order='diameters')       
+
+    # interpolate
+    if datapts['fractions'][0] > 0.0:
+
+        # if smallest fraction is not zero, interpolate linearly
+        # to find the missing value
+
+        D3 = log(datapts['diameters'][1],2)
+        D2 = log(datapts['diameters'][0],2)
+        f3 = datapts['fractions'][1]
+        f2 = datapts['fractions'][0]
+        newpsi = D3 + ((D2 - D3)/(f2 - f3))*(0-f3)
+        first_entry = np.array((2**newpsi,0), dtype=dtype)
+        datapts = np.hstack((first_entry, datapts))
+
+    if datapts['fractions'][-1] < 1.0:
+
+        # if largest fraction is not zero, assume that the next larger
+        # psi scale grain size had a fraction of 1
+
+        psi_scale = np.array(range(-10,20), dtype = np.float)
+        psi_scale_mm = 2**psi_scale
+
+        next_larger_size = \
+            psi_scale_mm[ np.where(psi_scale_mm > \
+                datapts['diameters'][-1])[0][0]]
+
+        last_entry = np.array([(next_larger_size, 1.0)], dtype = dtype)
+        datapts = np.hstack((datapts, last_entry))
+
+    return datapts 
+
+
+def grain_statistics(datapts):
+    '''
+    Statistical characteristics of grain size distributions
+
+    Input
+    -----
+    datapts: nd array of grain size diameters and percent finers
+
+    Output
+    ------
+    D_g: Geometric mean grain diameter in millimiter
+    sigma_g: Standard deviation of grain size in millimiters
+        Sediment is well sorted if sigma_g < 1.6
+    '''
+
+    psi_b = [log(i,2) for i in datapts['diameters']]
+
+    dtype = [('psi', float), ('diameters', float), ('fractions', float)]
+    centerpts = np.zeros((len(datapts)-1), dtype=dtype)
+
+    for i in range(len(centerpts)):    
+        centerpts['psi'][i] = (psi_b[i+1] + psi_b[i]) / 2
+        centerpts['diameters'][i] = sqrt(datapts['diameters'][i] * \
+                datapts['diameters'][i+1])
+        centerpts['fractions'][i] = datapts['fractions'][i+1] - \
+                datapts['fractions'][i]
+
+    psi_bar = sum(centerpts['psi'] * centerpts['fractions'])
+    D_g = 2 ** psi_bar
+
+    sigma = sqrt(sum((centerpts['psi'] - psi_bar)**2 * \
+        centerpts['fractions']))
+    sigma_g = 2 ** sigma
+
+    return D_g, sigma_g
 
 
 
@@ -40,7 +118,7 @@ class GSD_calculator(object):
         if verbose:
             print 'Initializing RouseVanoni_Equilibrium...'
             
-        self._datapts = [(4, 100), (2, 99), (1, 97), (0.5, 83.4), ...
+        self._datapts = [(4, 100), (2, 99), (1, 97), (0.5, 83.4), 
                          (0.25, 42), (0.125, 10), (0.062, 3.2), (0.031, 2)]
         
         self._D_g = None
@@ -66,7 +144,7 @@ class GSD_calculator(object):
         """
         
         self._datapoints = new_dp
-        self.pre_process_data()
+        self._datapts = pre_process_data(self._datapoints)
         
         
 
@@ -87,7 +165,7 @@ class GSD_calculator(object):
     
         assert self._datapts is not None, "Grain size data must be set before running."
         
-        self.grain_statistics()
+        self._D_g, self._sigma_g = grain_statistics(self._datapts)
         self.finalize()
         
         
@@ -105,33 +183,33 @@ class GSD_calculator(object):
         
     
     
-    def pre_process_data(self):
+    def pre_process_data(self, datapoints):
     
         dtype = [('diameters', float), ('fractions', float)]
-        self._datapts = np.array(self._datapoints, dtype=dtype)
+        datapts = np.array(datapoints, dtype=dtype)
 
         # scale
-        if max(self._datapts['fractions']) > 1.:
-            self._datapts['fractions'] = self._datapts['fractions'] / 100.
+        if max(datapts['fractions']) > 1.:
+            datapts['fractions'] = datapts['fractions'] / 100.
 
         # sort
-        self._datapts = np.sort(self._datapts, order='diameters')       
+        datapts = np.sort(datapts, order='diameters')       
 
         # interpolate
-        if self._datapts['fractions'][0] > 0.0:
+        if datapts['fractions'][0] > 0.0:
 
             # if smallest fraction is not zero, interpolate linearly
             # to find the missing value
     
-            D3 = log(self._datapts['diameters'][1],2)
-            D2 = log(self._datapts['diameters'][0],2)
-            f3 = self._datapts['fractions'][1]
-            f2 = self._datapts['fractions'][0]
+            D3 = log(datapts['diameters'][1],2)
+            D2 = log(datapts['diameters'][0],2)
+            f3 = datapts['fractions'][1]
+            f2 = datapts['fractions'][0]
             newpsi = D3 + ((D2 - D3)/(f2 - f3))*(0-f3)
             first_entry = np.array((2**newpsi,0), dtype=dtype)
-            self._datapts = np.hstack((first_entry, self._datapts))
+            datapts = np.hstack((first_entry, datapts))
     
-        if self._datapts['fractions'][-1] < 1.0:
+        if datapts['fractions'][-1] < 1.0:
 
             # if largest fraction is not zero, assume that the next larger
             # psi scale grain size had a fraction of 1
@@ -141,19 +219,21 @@ class GSD_calculator(object):
     
             next_larger_size = \
                 psi_scale_mm[ np.where(psi_scale_mm > \
-                    self._datapts['diameters'][-1])[0][0]]
+                    datapts['diameters'][-1])[0][0]]
                 
             last_entry = np.array([(next_larger_size, 1.0)], dtype = dtype)
-            self._datapts = np.hstack((self._datapts, last_entry))    
+            datapts = np.hstack((datapts, last_entry))
+            
+        return datapts 
     
 
-    def characteristic_size(self, pct):
+    def characteristic_size(self, datapts, pct):
         '''
         Calculate characteristic size based on percent finer
     
         Input
         ------
-        datapts_: nd array of grain size diameters and percent finers
+        datapts: nd array of grain size diameters and percent finers
         pct: percentage such that D_pct is the size such that pct percent
             of the sample is finer than D_pct (between 0 and 100)
         
@@ -170,60 +250,30 @@ class GSD_calculator(object):
             
         assert len(pct), 'pct is the wrong format!'
         
-        self.D_x = []
+        D_x = []
             
         for i in pct:
 
             pct_ = float(i)
     
-            loc_ = np.where(self._datapts['fractions'] >= pct_/100)[0][0]
+            loc_ = np.where(datapts['fractions'] >= pct_/100)[0][0]
             assert loc_>0, 'Percent finer appears to be less than 0!'
 
-            D2_ = log(self._datapts['diameters'][loc_],2)
-            D1_ = log(self._datapts['diameters'][loc_-1],2)
-            f2_ = self._datapts['fractions'][loc_]
-            f1_ = self._datapts['fractions'][loc_-1]
+            D2_ = log(datapts['diameters'][loc_],2)
+            D1_ = log(datapts['diameters'][loc_-1],2)
+            f2_ = datapts['fractions'][loc_]
+            f1_ = datapts['fractions'][loc_-1]
 
             psix_ = D1_ + ((D2_ - D1_)/(f2_ - f1_)) * (pct_/100 - f1_)
-            self.D_x.append(2**psix_)
+            D_x.append(2**psix_)
+            
+        return D_x
             
         
         
 
 
-    def grain_statistics(self):
-        '''
-        Statistical characteristics of grain size distributions
-    
-        Input
-        -----
-        datapts: nd array of grain size diameters and percent finers
-    
-        Output
-        ------
-        D_g: Geometric mean grain diameter in millimiter
-        sigma_g: Standard deviation of grain size in millimiters
-            Sediment is well sorted if sigma_g < 1.6
-        '''
 
-        psi_b = [log(i,2) for i in self._datapts['diameters']]
-
-        dtype = [('psi', float), ('diameters', float), ('fractions', float)]
-        self._centerpts = np.zeros((len(self._datapts)-1), dtype=dtype)
-
-        for i in range(len(self._centerpts)):    
-            self._centerpts['psi'][i] = (psi_b[i+1] + psi_b[i]) / 2
-            self._centerpts['diameters'][i] = sqrt(self._datapts['diameters'][i] * \
-                    self._datapts['diameters'][i+1])
-            self._centerpts['fractions'][i] = self._datapts['fractions'][i+1] - \
-                    self._datapts['fractions'][i]
-
-        self._psi_bar = sum(self._centerpts['psi'] * self._centerpts['fractions'])
-        self._D_g = 2 ** self._psi_bar
-
-        self._sigma = sqrt(sum((self._centerpts['psi'] - self._psi_bar)**2 * \
-            self._centerpts['fractions']))
-        self._sigma_g = 2 ** self._sigma      
             
             
             
